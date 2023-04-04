8a0d21beae39e8165d14ce6ae27d6054376d775f
vladislavBT <vladislavtulilov12345678@gmail.com>
2023-04-04 18:18:24 +0300

    Some frontEnd.

---
+++ src/main/java/com/softuni/shoestrade/web/HomeController.java
@@ -0,0 +1,17 @@
+package com.softuni.shoestrade.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController extends BaseController{
+
+    @GetMapping
+    public ModelAndView getHome() {
+        return super.view("log-in");
+    }
+
+}
