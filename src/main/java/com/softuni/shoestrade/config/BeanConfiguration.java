b46f639c4326544df2c29571635dfc9032926abf
vladislavBT <vladislavtulilov12345678@gmail.com>
2023-04-04 18:17:51 +0300

    Some front-end.

--- src/main/java/com/softuni/shoestrade/config/BeanConfiguration.java
+++ src/main/java/com/softuni/shoestrade/config/BeanConfiguration.java
@@ -1,7 +1,55 @@
 package com.softuni.shoestrade.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
 public class BeanConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity, SecurityContextRepository securityContextRepository) throws Exception {
+        httpSecurity.
+                // defines which pages will be authorized
+                        authorizeHttpRequests().
+                // allow access to all static files (images, CSS, js)
+                        requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
+                // the URL-s below are available for all users - logged in and anonymous
+                        requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll().
+                anyRequest().authenticated().
+                and().
+                // configure login with HTML form
+                        formLogin().
+                loginPage("/users/login").
+                // the names of the user name, password input fields in the custom login form
+                        usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
+                passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
+                // where do we go after login
+                        defaultSuccessUrl("/").//use true argument if you always want to go there, otherwise go to previous page
+                failureForwardUrl("/users/login-error").
+                and().logout().//configure logout
+                logoutUrl("/users/logout").
+                logoutSuccessUrl("/").//go to homepage after logout
+                invalidateHttpSession(true).
+                and().
+                securityContext().
+                securityContextRepository(securityContextRepository);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new DelegatingSecurityContextRepository(
+                new RequestAttributeSecurityContextRepository(),
+                new HttpSessionSecurityContextRepository()
+        );
+    }
 }
